7be24ffbfdc289cf93d11471b4ce06279c44b3ea
Andrea Stagi <stagi.andrea@gmail.com>
2020-03-03 19:11:08 +0100

    Add build

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,3 @@
-import Vue from 'vue'
 import ImagePlaceholder from './ImagePlaceholder.vue'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(
-    ImagePlaceholder,
-    {
-      props : {
-        width: 500,
-        height:200,
-        images: 'newyork'
-      }
-    }),
-}).$mount('#app')
+export default ImagePlaceholder
---
+++ types/index.d.ts
@@ -0,0 +1,4 @@
+declare module 'vue-image-placeholder' {
+  const placeholder: any;
+  export default placeholder;
+}
