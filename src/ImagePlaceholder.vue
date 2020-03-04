57741bcfd2bd229ba221c59ddcb3b1822ebf5689
Andrea Stagi <stagi.andrea@gmail.com>
2020-03-05 00:45:35 +0100

    Refactor base url

--- src/ImagePlaceholder.vue
+++ src/ImagePlaceholder.vue
@@ -24,10 +24,15 @@ export default class ImagePlaceholder extends Vue {
     }
   }
 
-  private getLoremFlickrUrl(): string {
-    const finalUrl = new URL("https://loremflickr.com/");
+  private getBaseUrl(host: string): URL {
+    const finalUrl = new URL(`https://${host}/`);
     finalUrl.pathname = `/${this.width}`;
     finalUrl.pathname += `/${this.height || this.width}`;
+    return finalUrl;
+  }
+
+  private getLoremFlickrUrl(): string {
+    const finalUrl = this.getBaseUrl('loremflickr.com');
     if (this.images.includes(',')) {
       finalUrl.pathname += `/${this.images}/all`;
     } else {
@@ -37,17 +42,11 @@ export default class ImagePlaceholder extends Vue {
   }
 
   private getBillMurrayUrl(): string {
-    const finalUrl = new URL("https://www.fillmurray.com/");
-    finalUrl.pathname = `/${this.width}`;
-    finalUrl.pathname += `/${this.height || this.width}`;
-    return finalUrl.href;
+    return this.getBaseUrl('www.fillmurray.com').href;
   }
 
   private getStevenSegalUrl(): string {
-    const finalUrl = new URL("https://www.stevensegallery.com/");
-    finalUrl.pathname = `/${this.width}`;
-    finalUrl.pathname += `/${this.height || this.width}`;
-    return finalUrl.href;
+    return this.getBaseUrl('www.stevensegallery.com').href;
   }
 
 }
